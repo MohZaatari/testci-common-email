32858b3b952fc624596b76c0bee6470aad802125
MohZaatari <91998180+MohZaatari@users.noreply.github.com>
2023-04-03 15:01:51 -0400

    added a comment to trigger CI

--- src/test/java/org/apache/commons/mail/EmailConcrete.java
+++ src/test/java/org/apache/commons/mail/EmailConcrete.java
@@ -4,7 +4,7 @@ public class EmailConcrete extends Email {
 //this class is created because email is abstract
 	//it is used for testing purpose 
 	@Override
-	public Email setMsg(String msg) throws EmailException {
+	public Email setMsg(String msg) throws EmailException {//setmsg method that sets the body of email
 		// TODO Auto-generated method stub
 		return null;//this method was not implemented in email, so for now we make it return null
 	}
